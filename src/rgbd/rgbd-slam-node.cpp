4fe52f3d19a79b1de1fd81f71ee310ae8c93d200
Vipipi <konamiv3@hotmail.com>
2025-07-24 15:36:54 -0700

    Update rgbd-slam-node.cpp

--- src/rgbd/rgbd-slam-node.cpp
+++ src/rgbd/rgbd-slam-node.cpp
@@ -7,15 +7,14 @@ RgbdSlamNode::RgbdSlamNode(const std::string& voc_path, const std::string& setti
 {
     rclcpp::QoS qos_profile(rclcpp::KeepLast(10), rmw_qos_profile_sensor_data);
 
+    // Initialize all three subscribers
     rgb_sub = std::make_shared<message_filters::Subscriber<ImageMsg>>(shared_ptr<rclcpp::Node>(this), "camera/rgb", qos_profile.get_rmw_qos_profile());
     depth_sub = std::make_shared<message_filters::Subscriber<ImageMsg>>(shared_ptr<rclcpp::Node>(this), "camera/depth", qos_profile.get_rmw_qos_profile());
+    info_sub = std::make_shared<message_filters::Subscriber<CameraInfoMsg>>(shared_ptr<rclcpp::Node>(this), "camera/camera_info", qos_profile.get_rmw_qos_profile());
 
-    // Create the synchronizer
-    syncApproximate = std::make_shared<message_filters::Synchronizer<approximate_sync_policy>>(approximate_sync_policy(10), *rgb_sub, *depth_sub);
+    // Create synchronizer for three topics
+    syncApproximate = std::make_shared<message_filters::Synchronizer<approximate_sync_policy>>(approximate_sync_policy(10), *rgb_sub, *depth_sub, *info_sub);
     
-    // Set the slop by creating an explicit rclcpp::Duration object
-    syncApproximate->setSlop(rclcpp::Duration::from_seconds(0.1));
-
     syncApproximate->registerCallback(&RgbdSlamNode::GrabRGBD, this);
 }
 
@@ -25,8 +24,12 @@ RgbdSlamNode::~RgbdSlamNode()
     m_SLAM->SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
 }
 
-void RgbdSlamNode::GrabRGBD(const ImageMsg::SharedPtr msgRGB, const ImageMsg::SharedPtr msgD)
+// Change function signature to accept three messages
+void RgbdSlamNode::GrabRGBD(const ImageMsg::SharedPtr msgRGB, const ImageMsg::SharedPtr msgD, const CameraInfoMsg::SharedPtr msg_info)
 {
+    // The camera_info message isn't used here, but its presence in the synchronizer is key
+    (void)msg_info;
+
     try
     {
         cv_ptrRGB = cv_bridge::toCvShare(msgRGB);
